933f4206a135a8935abaeeef8494d2009e3cf1a6
nj0802-alt <nj0802@srmist.edu.in>
2026-03-03 11:16:06 +0530

    initial commit

---
+++ UC3.java
@@ -0,0 +1,17 @@
+/**
+ * Printing OOPS
+ * @author nj0802-alt
+ * @version 1.1
+ */
+
+public class UC3{
+    public static void main(String[] args) {
+        System.out.println(String.join(" ", "*****", " ","*****", " ", "*****", " ", "*****"));
+        System.out.println(String.join(" ", "*   *", " ", "*   *", " ", "*   *", " ", "*    "));
+        System.out.println(String.join(" ", "*   *", " ", "*   *", " ", "*   *", " ", "*    "));
+        System.out.println(String.join(" ", "*   *", " ", "*   *", " ", "*****", " ", "*****"));
+        System.out.println(String.join(" ", "*   *", " ", "*   *", " ", "*    ", " ", "    *"));
+        System.out.println(String.join(" ", "*   *", " ", "*   *", " ", "*    ", " ", "    *"));
+        System.out.println(String.join(" ", "*****", " ", "*****", " ", "*    ", " ", "*****"));                
+    }
+}
